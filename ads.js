c445c203e95ef17b416f38580ef3cc97da6663a6
leomartis <Lfermino044@gmail.com>
2025-03-09 23:44:59 -0300

    projeto inicial

--- ads.js
+++ ads.js
@@ -2,6 +2,5 @@ document.querySelectorAll('.comprar-btn').forEach(button => {
     button.addEventListener('click', (e) => {
         const skinId = e.target.getAttribute('data-skin');
         alert(`Você comprou a skin com ID: ${skinId}`);
-        // Aqui você pode fazer uma requisição para o backend
     });
 });
